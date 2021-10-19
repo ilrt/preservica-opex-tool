de10b548b25ba8c4093c75dddabd3839cfabae3d
Damian Steer <d.steer@bris.ac.uk>
2021-10-19 11:25:01 +0100

    Fix the way I work out source ids

--- generate_opex.py
+++ generate_opex.py
@@ -1,28 +1,39 @@
 import os
 import xml.etree.ElementTree as ET
-
-from os.path import join, splitext, basename
+import os.path as path
+from pathlib import Path
 
 opex = "http://www.openpreservationexchange.org/opex/v1.0"
 ET.register_namespace("opex", opex)
 
 def ignore(file):
-	name, ext = splitext(file)
+	name, ext = path.splitext(file)
 	return ext in ['.md5', '.opex']
 
-def get_content(root, file):
-	with open(join(root, file)) as f: s = f.read()
+def get_content(file):
+	with open(file) as f: s = f.read()
 	return s
+
+# We expect the source dir to be already flattened
+# with each item in the root dir with the calm id
+def get_source_id(dir):
+	path = Path(dir).parts
+	if len(path) < 2:
+		return None
+	else:
+		return path[1]
 
 def output_dir(root, dirs, files):
 	root_elem = ET.Element(f"{{{opex}}}OPEXMetadata")
 	transfer = ET.SubElement(root_elem, f"{{{opex}}}Transfer")
 
-	base = basename(root)
+	base = path.basename(root)
 
-	source_id = ET.SubElement(transfer, f"{{{opex}}}SourceID")
+	source_id = get_source_id(root)
 
-	source_id.text = base
+	if source_id:
+		source_id_elem = ET.SubElement(transfer, f"{{{opex}}}SourceID")
+		source_id_elem.text = source_id
 
 	manifest_elem = ET.SubElement(transfer, f"{{{opex}}}Manifest")
 	files_elem = ET.SubElement(manifest_elem, f"{{{opex}}}Files")
@@ -50,21 +61,23 @@ def output_dir(root, dirs, files):
 	root_tree.write(root + "/" + base + ".opex")
 
 def get_md5(filename):
-	name, ext = splitext(filename)
+	name, ext = path.splitext(filename)
 	md5file = name + '.md5'
 	if path.exists(md5file):
 		return get_content(md5file)
 	elif path.islink(filename):
-		return get_md5(path.abspath(filename))
+		link = Path(filename)
+		target = link.resolve()
+		return get_md5(str(target))
 	else:
 		return None
 
 def output_file(root, file, files):
 	root_elem = ET.Element(f"{{{opex}}}OPEXMetadata")
-	filename = join(root, file)
+	filename = path.join(root, file)
 	md5 = get_md5(filename)
 	if md5:
-		md5 = get_content(root, md5file)
+		transfer = ET.SubElement(root_elem, f"{{{opex}}}Transfer")
 		fixities = ET.SubElement(transfer, f"{{{opex}}}Fixities")
 		ET.SubElement(fixities, f"{{{opex}}}Fixity", type="MD5", value=md5)
 	else:
@@ -74,11 +87,11 @@ def output_file(root, file, files):
 	ET.indent(root_tree)
 	root_tree.write(filename + ".opex")
 
-for root, dirs, files in os.walk('test-pax'):
+for root, dirs, files in os.walk('FB-flattened'):
 
 	output_dir(root, dirs, files)
 
-	id = basename(root)
+	id = path.basename(root)
 
 	for file in files:
 		if ignore(file):
