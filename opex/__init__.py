45000d7d74beaf4e3d0b6f7c97ae901202c0cfdd
Damian Steer <d.steer@bris.ac.uk>
2022-06-13 12:09:06 +0100

    Tab / space mess up

--- opex/__init__.py
+++ opex/__init__.py
@@ -156,9 +156,9 @@ def get_md5(filename):
 			file_hash = hashlib.md5()
 			chunk = f.read(8192)
 			while chunk:
-        		file_hash.update(chunk)
-        		chunk = f.read(8192)
-  			return file_hash.hexdigest()
+				file_hash.update(chunk)
+				chunk = f.read(8192)
+			return file_hash.hexdigest()
 
 def output_pax_file(root, file):
 
